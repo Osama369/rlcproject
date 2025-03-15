c1fec54fe79dbfc133b76c828e7de89dfc134d9f
Mubashir Ali <21sw071@students.muet.edu.pk>
2025-03-15 17:55:00 +0500

    added CRUD operations for users

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,10 +1,42 @@
-import express from 'express';
-import { getAllUsers, createUser } from '../controllers/userController.js';
-import { authMiddleware , adminMiddleware } from '../middlewares/authMiddleware.js';
+import express from "express";
+import {
+  getAllUsers,
+  createUser,
+  getUserById,
+  initializeUserBalance,
+  deductUserBalance,
+  deleteUser,
+  updateUser,
+  toggleUserActiveStatus,
+} from "../controllers/userController.js";
+import {
+  authMiddleware,
+  adminMiddleware,
+} from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.get('/', authMiddleware, getAllUsers);
-router.post('/', authMiddleware, adminMiddleware, createUser);
-
+router.get("/", authMiddleware, adminMiddleware, getAllUsers);
+router.post("/", authMiddleware, adminMiddleware, createUser);
+router.get("/:id", authMiddleware, adminMiddleware, getUserById);
+router.put("/:id", authMiddleware, adminMiddleware, updateUser);
+router.delete("/:id", authMiddleware, adminMiddleware, deleteUser);
+router.patch(
+  "/:id/active",
+  authMiddleware,
+  adminMiddleware,
+  toggleUserActiveStatus
+);
+router.patch(
+  "/:id/balance/initialize",
+  authMiddleware,
+  adminMiddleware,
+  initializeUserBalance
+);
+router.patch(
+  "/:id/balance/deduct",
+  authMiddleware,
+  adminMiddleware,
+  deductUserBalance
+);
 export default router;
